39e278bc17a7aa04da69fe7fdf2599e012db957e
Kalhar Pandya <kalhar.p@asambhav.in>
2021-03-10 04:46:37 +0530

    fix:memory Issue

--- ArduinoJSON.h
+++ ArduinoJSON.h
@@ -1,85 +1,116 @@
 #include "JsonType/JsonType.h"
+#include <string.h>
 json parsed;
-int knowTokenType(String token){
-    bool isNumber = true;
-    bool isDouble = true;
-    bool isString = true;
-    for(int i = 0 ; i < token.length(); i ++){
-        if(!isDigit(token[i])){
+
+int found = 0;
+int strIndex[] = {0, -1};
+int maxIndex = 0;
+bool isNumber = true;
+bool isDouble = true;
+bool isString = true;
+long result = 0;
+String value;
+String key;
+
+int knowTokenType(String token)
+{
+    isNumber = true;
+    isDouble = true;
+    isString = true;
+    for (int i = 0; i < token.length(); i++)
+    {
+        if (!isDigit(token[i]))
+        {
             isNumber = false;
-            if(token[i]!='.'){
+            if (token[i] != '.')
+            {
                 isDouble = false;
                 return STRING;
             }
         }
     }
-    if(isNumber)
+    if (isNumber)
         return NUMBER;
-    if(isDouble)
+    if (isDouble)
         return DOUBLE;
-    if(isString)
+    if (isString)
         return STRING;
     return -1;
 }
 
-long getNumber(String token){
-    long result = 0;
-    for(int i =0;i< token.length(); i ++){
-        result= result*10 + (token[i]-'0');
+long getNumber(String token)
+{
+    result = 0;
+    for (int i = 0; i < token.length(); i++)
+    {
+        result = result * 10 + (token[i] - '0');
     }
     return result;
 }
-double getDouble(String token){
+double getDouble(String token)
+{
     return token.toDouble();
 }
 
-String strSplit(String data, char separator, int index)
+String strSplit(String data,  char separator, int index)
 {
-  int found = 0;
-  int strIndex[] = {0, -1};
-  int maxIndex = data.length()-1;
-
-  for(int i=0; i<=maxIndex && found<=index; i++){
-    if(data.charAt(i)==separator || i==maxIndex){
-        found++;
-        strIndex[0] = strIndex[1]+1;
-        strIndex[1] = (i == maxIndex) ? i+1 : i;
+    // Serial.println("Splitting   "+data);
+    int index_cnt = -1;
+    data += separator;
+    String token = "";
+    for(int i = 0 ; i < data.length(); i ++){
+        if(data[i] == separator){
+            index_cnt+=1;
+            if(index_cnt == index){
+                return token;
+            }
+            token = "";
+        }
+        else
+            token+=data[i];
     }
-  }
-
-  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
-void createUnit(String token){
-    String key = strSplit(token,':',0);
-    key.remove(0,1);
-    key.remove(key.length()-1,1);
-    String value = strSplit(token,':',1);
+void createUnit(String token)
+{
+    key = strSplit(token, ':', 0);
+    
+    key.remove(0, 1);
+    key.remove(key.length() - 1, 1);
+    value = strSplit(token, ':', 1);
+    
+    // Serial.println("Value = " + value);
     switch (knowTokenType(value))
     {
     case STRING:
-        value.remove(0,1);
-        value.remove(value.length()-1,1);
-        parsed.addUnit(key,value);
+        value.remove(0, 1);
+        value.remove(value.length() - 1, 1);
+        parsed.addUnit(key, value);
         break;
     case NUMBER:
-        parsed.addUnit(key,getNumber(value));
+        parsed.addUnit(key, getNumber(value));
         break;
     case DOUBLE:
-        parsed.addUnit(key,double(getDouble(value)));
+        parsed.addUnit(key, double(getDouble(value)));
         break;
     default:
-        parsed.addUnit(key,value);
+        parsed.addUnit(key, value);
     }
 }
-json parseJSON(String jsonStr){
-    jsonStr.remove(0,1);
-    jsonStr.remove(jsonStr.length()-1,1);
+json parseJSON(String jsonStr)
+{
+    jsonStr.remove(0, 1);
+    jsonStr.remove(jsonStr.length() - 1, 1);
     // Serial.println(jsonStr);
     parsed.clear();
     int i = 0;
-    while(strSplit(jsonStr,',',i).length()!=0){
-        createUnit(strSplit(jsonStr,',',i));
+    String token;
+    do
+    {   
+        token = strSplit(jsonStr, ',', i);
+        if(token == "")
+            break;
+        createUnit(token);
         i++;
-    }
+    }while (token != "");
     return parsed;
 }
--- JsonType/JsonType.h
+++ JsonType/JsonType.h
@@ -1,104 +1,132 @@
 #include "./unit/unit.h"
-#define MAX_UNITS 100
+#define MAX_UNITS 10
 unit invalidUnit;
-class json{
-    public:
+class json
+{
+public:
     int index = 0;
     unit *units[MAX_UNITS];
     String result;
-    json(){
+    json()
+    {
         invalidUnit.setKey("Invalid");
         invalidUnit.value->double_data = -1;
         invalidUnit.value->number_data = -1;
         invalidUnit.value->str_data = "";
+        for (int i = 0; i < MAX_UNITS; i++)
+        {
+
+            units[i] = new unit();
+        }
     }
-    void addUnit(String key, String value){
-        units[index] = new unit();
+    void addUnit(String key, String value)
+    {
         units[index]->setKey(key);
         units[index]->setValue(value);
         index++;
     }
-    void addUnit(String key, double value){
-        units[index] = new unit();
+    void addUnit(String key, double value)
+    {
         units[index]->setKey(key);
         units[index]->setValue(value);
         index++;
     }
-    void addUnit(String key, long value){
-        units[index] = new unit();
+    void addUnit(String key, long value)
+    {
         units[index]->setKey(key);
         units[index]->setValue(value);
         index++;
     }
-    void addUnit(String key, int value){
-        addUnit(key,long(value));
+    void addUnit(String key, int value)
+    {
+        addUnit(key, long(value));
     }
-    void addUnit(String key, float value){
-        addUnit(key,double(value));
+    void addUnit(String key, float value)
+    {
+        addUnit(key, double(value));
     }
-    unit* findUnit(String key){
-        for(int i = 0 ; i < index ; i ++){
-            if(units[i]->getKey() == key){
+    unit *findUnit(String key)
+    {
+        for (int i = 0; i < index; i++)
+        {
+            if (units[i]->getKey() == key)
+            {
                 return units[i];
             }
         }
         return &invalidUnit;
     }
-    String getValue(String key){
-        unit * found_unit = findUnit(key);
+    String getValue(String key)
+    {
+        unit *found_unit = findUnit(key);
         return found_unit->value->str_data;
     }
-    long getNumberValue(String key){
-        unit * found_unit = findUnit(key);
+    long getNumberValue(String key)
+    {
+        unit *found_unit = findUnit(key);
         return found_unit->value->number_data;
     }
-    double getDoubleValue(String key){
-        unit * found_unit = findUnit(key);
+    double getDoubleValue(String key)
+    {
+        unit *found_unit = findUnit(key);
         return found_unit->value->double_data;
     }
-    void clear(){
+    void clear()
+    {
         index = 0;
     }
-    void updateValue(String key, String Value){
-        unit * found_unit = findUnit(key);
+    void updateValue(String key, String Value)
+    {
+        unit *found_unit = findUnit(key);
         found_unit->setValue(Value);
     }
-    void updateValue(String key, long Value){
-        unit * found_unit = findUnit(key);
+    void updateValue(String key, long Value)
+    {
+        unit *found_unit = findUnit(key);
         found_unit->setValue(Value);
     }
-    void updateValue(String key, double Value){
-        unit * found_unit = findUnit(key);
+    void updateValue(String key, double Value)
+    {
+        unit *found_unit = findUnit(key);
         found_unit->setValue(Value);
     }
-    
-    void updateValue(String key, int Value){
-        updateValue(key,long(Value));
+
+    void updateValue(String key, int Value)
+    {
+        updateValue(key, long(Value));
     }
-    void updateValue(String key, float Value){
-        updateValue(key,double(Value));
+    void updateValue(String key, float Value)
+    {
+        updateValue(key, double(Value));
     }
-    void remove(String key){
-        for(int i = 0 ; i < index ; i ++){
-            if(units[i]->getKey() == key){
-                for(int j = i; j < index-1; j ++){
-                    units[j] = units[j+1];
+    void remove(String key)
+    {
+        for (int i = 0; i < index; i++)
+        {
+            if (units[i]->getKey() == key)
+            {
+                for (int j = i; j < index - 1; j++)
+                {
+                    units[j] = units[j + 1];
                 }
-                index-=1;
+                index -= 1;
                 break;
             }
         }
     }
-    String getString(bool withEscape = false){
+    String getString(bool withEscape = false)
+    {
         result = "{";
-        for(int i = 0 ; i < index ; i ++){
-            result+=units[i]->getString(withEscape);
-            if(i!=index-1){
-                result+=',';
+        for (int i = 0; i < index; i++)
+        {
+            result += units[i]->getString(withEscape);
+            if (i != index - 1)
+            {
+                result += ',';
             }
         }
-        result+="}";
-        String esc = withEscape?"\"":"";
-        return esc+result+esc;
+        result += "}";
+        String esc = withEscape ? "\"" : "";
+        return esc + result + esc;
     }
 };
