3016de78f199d1594fb266ba1a07bf7cbed2927f
Kalhar Pandya <kalhar.p@asambhav.in>
2021-03-11 19:02:24 +0530

    bugFix in findJSON

--- ArduinoJSON.h
+++ ArduinoJSON.h
@@ -107,7 +107,7 @@ String findJSON(String raw, char start = '{', char finish='}')
     {
         if (raw[i] == start)
         {
-            while (raw[i] != finish)
+            while (raw[i-1] != finish)
             {
                 json += raw[i];
                 i++;
@@ -122,6 +122,8 @@ String findJSON(String raw, char start = '{', char finish='}')
 json parseJSON(String jsonStr)
 {
     jsonStr = findJSON(jsonStr);
+    
+        Serial.println(jsonStr);
     jsonStr.remove(0, 1);
     jsonStr.remove(jsonStr.length() - 1, 1);
     parsed.clear();
